090fbc0bb125dd9fdeac0181fd9d4dfd1ce31282
caio86 <80562020+caio86@users.noreply.github.com>
2023-11-20 23:40:43 -0300

    feat: add new repo methods to fornecedorRepository

--- src/main/java/com/brasileiras/api/fornecedor/FornecedorRepository.java
+++ src/main/java/com/brasileiras/api/fornecedor/FornecedorRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+  Fornecedor findByCnpj(String cnpj);
+
+  Boolean existsByCnpj(String cnpj);
 }
