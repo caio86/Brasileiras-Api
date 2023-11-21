679b1b2f5120591e86a5eef129f719550992cb18
caio86 <80562020+caio86@users.noreply.github.com>
2023-11-21 00:40:45 -0300

    refactor: change repository type to return Optional

--- src/main/java/com/brasileiras/api/fornecedor/FornecedorRepository.java
+++ src/main/java/com/brasileiras/api/fornecedor/FornecedorRepository.java
@@ -1,11 +1,13 @@
 package com.brasileiras.api.fornecedor;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-  Fornecedor findByCnpj(String cnpj);
+  Optional<Fornecedor> findByCnpj(String cnpj);
 
   Boolean existsByCnpj(String cnpj);
 }
--- src/main/java/com/brasileiras/api/produto/ProdutoRepository.java
+++ src/main/java/com/brasileiras/api/produto/ProdutoRepository.java
@@ -1,11 +1,13 @@
 package com.brasileiras.api.produto;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-  Produto findByCodigoBarras(long codigoBarras);
+  Optional<Produto> findByCodigoBarras(long codigoBarras);
 
   Boolean existsByCodigoBarras(long codigoBarras);
 }
