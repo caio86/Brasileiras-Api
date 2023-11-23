e3b0c8085c9c4b992180c31bc8b3aea2f8370429
caio86 <80562020+caio86@users.noreply.github.com>
2023-11-23 17:05:53 -0300

    feat: create obterEstoque getMapping

--- src/main/java/com/brasileiras/api/produto/ProdutoController.java
+++ src/main/java/com/brasileiras/api/produto/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +22,14 @@ public class ProdutoController {
   }
 
   @GetMapping("/produtos")
-  public ResponseEntity<List<Produto>> getAll() {
+  public ResponseEntity<List<Produto>> obterProdutos() {
     List<Produto> produtos = service.getAll();
     return new ResponseEntity<>(produtos, HttpStatus.OK);
+  }
+
+  @GetMapping("/estoque/{id}")
+  public ResponseEntity<Integer> obterEstoque(@PathVariable Long id) {
+    int estoque = service.getEstoque(id);
+    return new ResponseEntity<>(estoque, HttpStatus.OK);
   }
 }
