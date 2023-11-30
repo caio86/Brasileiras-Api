bfb90cfa10c1d239b514dc96981eaf6cdfa1bba0
caio86 <80562020+caio86@users.noreply.github.com>
2023-11-30 16:46:26 -0300

    refactor: use NotaFiscalService on AppConfig, instead of repository

--- src/main/java/com/brasileiras/api/AppConfig.java
+++ src/main/java/com/brasileiras/api/AppConfig.java
@@ -9,19 +9,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.brasileiras.api.fornecedor.Fornecedor;
-import com.brasileiras.api.fornecedor.FornecedorRepository;
 import com.brasileiras.api.notafiscal.NotaFiscal;
-import com.brasileiras.api.notafiscal.NotaFiscalRepository;
+import com.brasileiras.api.notafiscal.NotaFiscalService;
 import com.brasileiras.api.notafiscal.dto.NotaFiscalDTO;
 import com.brasileiras.api.notafiscal.dto.NotaFiscalProdutoDTO;
-import com.brasileiras.api.produto.ProdutoRepository;
 
 @Configuration
 public class AppConfig {
 
   @Bean
-  CommandLineRunner commandLineRunner(ProdutoRepository produtoRepository, FornecedorRepository fornecedorRepository,
-      NotaFiscalRepository notaFiscalRepository) {
+  CommandLineRunner commandLineRunner(NotaFiscalService notaFiscalService) {
     return args -> {
       Fornecedor f1 = new Fornecedor("Coca-cola", "123456789");
       NotaFiscalProdutoDTO nfp1 = new NotaFiscalProdutoDTO(10, 10.0, "Refri Cola 1l", 135798642L);
@@ -29,9 +26,11 @@ public class AppConfig {
       NotaFiscalDTO nfdto1 = new NotaFiscalDTO(LocalDate.of(2023, Month.NOVEMBER, 19), 150.0, List.of(nfp1, nfp2), f1);
       NotaFiscal nf1 = nfdto1.gerarNotaFiscal();
 
-      fornecedorRepository.save(f1);
-      produtoRepository.saveAll(nf1.getProdutos());
-      notaFiscalRepository.save(nf1);
+      // fornecedorRepository.save(f1);
+      // produtoRepository.saveAll(nf1.getProdutos());
+      // notaFiscalRepository.save(nf1);
+
+      notaFiscalService.create(nf1);
     };
   }
 }
